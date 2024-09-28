37b68d47b3ad4479fad7d9f78da3c17cec2b8fdf
ronaldophc <ronaldohortmann02@gmail.com>
2024-09-28 15:07:51 -0300

    fixed text

--- components/admin/settings/Index.vue
+++ components/admin/settings/Index.vue
@@ -3,11 +3,11 @@ import { ref } from 'vue';
 import SettingsForm from './SettingsForm.vue';
 import SettingsHours from './SettingsHours.vue';
 
-const isSmallScreen = ref(window.innerWidth < 768);
+const isSmallScreen = ref(window.innerWidth < 1025);
 const selectedComponent = ref('form');
 
 window.addEventListener('resize', () => {
-  isSmallScreen.value = window.innerWidth < 768;
+  isSmallScreen.value = window.innerWidth < 1025;
 });
 
 const showForm = () => {
@@ -20,15 +20,15 @@ const showHours = () => {
 </script>
 
 <template>
-  <div class="admin-settings_body flex-1 flex flex-col md:flex-row">
+  <div class="admin-settings_body flex-1 flex flex-col lg:flex-row">
     <div v-if="isSmallScreen" class="admin-settings_menu flex justify-center gap-4 bg-blue-200 p-2">
-      <button @click="showForm" :class="{'text-blue-600': selectedComponent === 'form'}" class="px-4 py-2">Settings Form</button>
-      <button @click="showHours" :class="{'text-blue-600': selectedComponent === 'hours'}" class="px-4 py-2">Settings Hours</button>
+      <button @click="showForm" :class="{'text-blue-600': selectedComponent === 'form'}" class="px-4 py-2">Informações</button>
+      <button @click="showHours" :class="{'text-blue-600': selectedComponent === 'hours'}" class="px-4 py-2">Horários</button>
     </div>
-    <div v-if="!isSmallScreen || selectedComponent === 'form'" class="admin-settings_form flex-1 flex flex-col md:flex-row">
+    <div v-if="!isSmallScreen || selectedComponent === 'form'" class="admin-settings_form flex-1 flex">
       <SettingsForm/>
     </div>
-    <div v-if="!isSmallScreen || selectedComponent === 'hours'" class="admin-settings_hours flex-1 flex justify-center flex-col md:flex-row">
+    <div v-if="!isSmallScreen || selectedComponent === 'hours'" class="admin-settings_hours flex-1 flex justify-center">
       <SettingsHours/>
     </div>
   </div>
